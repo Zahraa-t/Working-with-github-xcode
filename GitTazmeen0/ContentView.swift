59f6e38446fe85ebf54c7e11bf4cc956dcc8552b
Zahraa <zahraa.tazmeen@gmail.com>
2026-04-21 19:10:13 -0400

    Initial Commit

---
+++ GitTazmeen0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitTazmeen0
+//
+//  Created by Zahraa Tazmeen on 4/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTazmeen0/GitTazmeen0App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTazmeen0App.swift
+//  GitTazmeen0
+//
+//  Created by Zahraa Tazmeen on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct GitTazmeen0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
