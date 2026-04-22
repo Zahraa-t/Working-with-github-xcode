282e877da7c64bcab598e9beed728c99a414bed9
BT_C09_24 <mobileappdev@bths.edu>
2026-04-22 14:37:34 -0400

    Changed text and image

--- GitTazmeen0/ContentView.swift
+++ GitTazmeen0/ContentView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "circle")
+            Image(systemName: "circle.fill")
                 .imageScale(.large)
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Fun with git")
+            Text("Fun with github")
             Text("Modifying text will add an M to your file")
         }
         .padding()
