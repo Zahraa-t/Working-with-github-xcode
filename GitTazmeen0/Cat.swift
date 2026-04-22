ea9c5581324a4f8e8f02c0d0dad10a9de3a8dc20
BT_C09_24 <mobileappdev@bths.edu>
2026-04-22 14:40:51 -0400

    Added text to a file

--- GitTazmeen0/Cat.swift
+++ GitTazmeen0/Cat.swift
@@ -5,3 +5,13 @@
 //  Created by Zahraa Tazmeen on 4/21/26.
 //
 
+import SwiftUI
+
+struct Cat: View {
+    var body: some View {
+        VStack {
+            Text("This is cat file")
+        }
+        .padding()
+    }
+}
