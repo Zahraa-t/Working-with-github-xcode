350e9fc7e1a801a62d01469f5bb692b10c7f464a
Zahraa <zahraa.tazmeen@gmail.com>
2026-04-21 23:02:19 -0400

    added SampleView file

---
+++ GitTazmeen0/SampleView.swift
@@ -0,0 +1,21 @@
+//
+//  SampleView.swift
+//  GitTazmeen0
+//
+//  Created by Zahraa Tazmeen on 4/21/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        VStack {
+            Text("This is a sample")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SampleView()
+}
